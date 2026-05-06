ac3cdc66f5f187a6c7dab1042a097e7aad2fa9dd
Guilherme Marques <guilhermehenrique12308@gmail.com>
2026-05-06 17:26:55 -0300

    feat: add user entity and use case interfaces

---
+++ src/domain/entities/user.ts
@@ -0,0 +1,7 @@
+export type User = {
+  userId: string
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+}
---
+++ src/domain/use-cases/users/index.ts
@@ -0,0 +1,2 @@
+export { ICreateUserUseCase } from './i-create-user'
+export { IAuthenticateUserUseCase } from './i-authenticate-user'
