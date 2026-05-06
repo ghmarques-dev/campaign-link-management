8935aab9d8d91ca961a91aa345493f601f500cb0
Guilherme Marques <guilhermehenrique12308@gmail.com>
2026-05-06 17:26:58 -0300

    feat: add users and hash repository protocols and domain errors

---
+++ src/application/errors/errors/index.ts
@@ -0,0 +1,2 @@
+export { EmailAlreadyExistError } from './email-already-exists-error'
+export { InvalidCredentialsError } from './invalid-credentials-error'
---
+++ src/application/repositories/database/users-repository.ts
@@ -0,0 +1,22 @@
+import { User } from '@/domain/entities'
+
+export type UsersRepository = {
+  create(input: UsersRepository.Create.Input): UsersRepository.Create.Output
+  findByEmail(input: UsersRepository.FindByEmail.Input): UsersRepository.FindByEmail.Output
+  findById(input: UsersRepository.FindById.Input): UsersRepository.FindById.Output
+}
+
+export namespace UsersRepository {
+  export namespace Create {
+    export type Input = { name: string; email: string; password: string }
+    export type Output = Promise<User>
+  }
+  export namespace FindByEmail {
+    export type Input = { email: string }
+    export type Output = Promise<User | null>
+  }
+  export namespace FindById {
+    export type Input = { userId: string }
+    export type Output = Promise<User | null>
+  }
+}
